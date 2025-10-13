f551bdd1111b8d3a2d00a40bc98809ea4f147db4
= <=>
2025-10-13 14:58:51 -0300

    teste

--- src/main/java/com/devsuperior/aula/services/CategoriaService.java
+++ src/main/java/com/devsuperior/aula/services/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.aula.entities.Categoria;
 import com.devsuperior.aula.repositories.CategoriaRepository;
@@ -11,9 +12,11 @@ import com.devsuperior.aula.repositories.CategoriaRepository;
 @Service
 public class CategoriaService {
 
+	
 	@Autowired
 	private CategoriaRepository catRep;
 	
+	@Transactional(readOnly = true)
 	public List<Categoria> findAll() {
 		List<Categoria> categorias = catRep.findAll();
 		return categorias;
